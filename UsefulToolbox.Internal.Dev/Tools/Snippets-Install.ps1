c32f49affe41e9d063a232b1f33bcfc3c6409597
igor <eroshkin.igor.ev@gmail.com>
2018-03-09 14:31:08 +0300

    Add .editorconfig

--- UsefulToolbox.Internal.Dev/Tools/Snippets-Install.ps1
+++ UsefulToolbox.Internal.Dev/Tools/Snippets-Install.ps1
@@ -1,24 +1,24 @@
-﻿$snippetsPath = "$PSScriptRoot\..\CodeSnippets"
+$snippetsPath = "$PSScriptRoot\..\CodeSnippets"
 $documentsPath = [Environment]::getFolderPath("MyDocuments")
 
 $vsPaths = Get-ChildItem -Path $documentsPath -Directory -Filter "Visual Studio *"
 foreach ($vsPath in $vsPaths)
 {
-  $vsSnippetsPath = $vsPath.FullName + "\Code Snippets\Visual C#"
-  if (Test-Path $vsSnippetsPath)
-  {
-    $snippetsTargetPath = "$vsSnippetsPath\UsefulToolbox"
+	$vsSnippetsPath = $vsPath.FullName + "\Code Snippets\Visual C#"
+	if (Test-Path $vsSnippetsPath)
+	{
+		$snippetsTargetPath = "$vsSnippetsPath\UsefulToolbox"
 
-    if (Test-Path $snippetsTargetPath)
-    {
-      Get-ChildItem -Path $snippetsTargetPath -Include "*.snippet" -File | foreach { $_.Delete()}
-    }
-    else 
-    {
-      New-Item -ItemType Directory -Path $snippetsTargetPath | Out-Null
-    }
+		if (Test-Path $snippetsTargetPath)
+		{
+			Get-ChildItem -Path $snippetsTargetPath -Include "*.snippet" -File | foreach { $_.Delete()}
+		}
+		else
+		{
+			New-Item -ItemType Directory -Path $snippetsTargetPath | Out-Null
+		}
 
-    Write-Host "Importing Useful Toolbox code snippets from $snippetsPath to $snippetsTargetPath"
-    Copy-Item -Path "$snippetsPath\*.snippet" -Destination $snippetsTargetPath
-  }
+		Write-Host "Importing Useful Toolbox code snippets from $snippetsPath to $snippetsTargetPath"
+		Copy-Item -Path "$snippetsPath\*.snippet" -Destination $snippetsTargetPath
+	}
 }
--- UsefulToolbox.Internal.Dev/Tools/Snippets-Uninstall.ps1
+++ UsefulToolbox.Internal.Dev/Tools/Snippets-Uninstall.ps1
@@ -1,18 +1,18 @@
-﻿$documentsPath = [Environment]::getFolderPath("MyDocuments")
+$documentsPath = [Environment]::getFolderPath("MyDocuments")
 
 $vsPaths = Get-ChildItem -Path $documentsPath -Directory -Filter "Visual Studio *"
 foreach ($vsPath in $vsPaths)
 {
-  $vsSnippetsPath = $vsPath.FullName + "\Code Snippets\Visual C#"
-  if (Test-Path $vsSnippetsPath)
-  {
-    $snippetsTargetPath = "$vsSnippetsPath\UsefulToolbox"
+	$vsSnippetsPath = $vsPath.FullName + "\Code Snippets\Visual C#"
+	if (Test-Path $vsSnippetsPath)
+	{
+		$snippetsTargetPath = "$vsSnippetsPath\UsefulToolbox"
 
-    if (Test-Path $snippetsTargetPath)
-    {
-      Write-Host "Remove Useful Toolbox code snippets from $snippetsTargetPath"
-      Get-ChildItem -Path $snippetsTargetPath -Include "*.snippet" -File | foreach { $_.Delete()}
-      Remove-Item -Path $snippetsTargetPath -Force -Recurse
-    }
-  }
+		if (Test-Path $snippetsTargetPath)
+		{
+			Write-Host "Remove Useful Toolbox code snippets from $snippetsTargetPath"
+			Get-ChildItem -Path $snippetsTargetPath -Include "*.snippet" -File | foreach { $_.Delete()}
+			Remove-Item -Path $snippetsTargetPath -Force -Recurse
+		}
+	}
 }
