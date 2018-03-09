baf449557e7827b8893918d9e66cf307383d0712
igor <eroshkin.igor.ev@gmail.com>
2018-03-09 09:52:16 +0300

    Add Snippets-Uninstall.ps1

---
+++ UsefulToolbox.Internal.Dev/Tools/Snippets-Uninstall.ps1
@@ -0,0 +1,18 @@
+﻿$documentsPath = [Environment]::getFolderPath("MyDocuments")
+
+$vsPaths = Get-ChildItem -Path $documentsPath -Directory -Filter "Visual Studio *"
+foreach ($vsPath in $vsPaths)
+{
+  $vsSnippetsPath = $vsPath.FullName + "\Code Snippets\Visual C#"
+  if (Test-Path $vsSnippetsPath)
+  {
+    $snippetsTargetPath = "$vsSnippetsPath\UsefulToolbox"
+
+    if (Test-Path $snippetsTargetPath)
+    {
+      Write-Host "Remove Useful Toolbox code snippets from $snippetsTargetPath"
+      Get-ChildItem -Path $snippetsTargetPath -Include "*.snippet" -File | foreach { $_.Delete()}
+      Remove-Item -Path $snippetsTargetPath
+    }
+  }
+}
